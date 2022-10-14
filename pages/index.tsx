a792239b43ddcc34f64333b639ca12b1666e52f5
gilpop8663 <wolfye@naver.com>
2022-10-14 15:43:50 +0900

    Edit:클립보드 안드로이드 , ios , 컴퓨터 각각의 상황 지원

--- pages/index.tsx
+++ pages/index.tsx
@@ -264,8 +264,31 @@ function FifthSection() {
 
   const handleCopyText = async (text: string) => {
     try {
-      await navigator.clipboard.writeText(text);
-      alert("복사가 되었습니다.");
+      if (navigator.clipboard) {
+        await navigator.clipboard.writeText(text);
+        alert("클립보드에 복사되었습니다.");
+      } else {
+        if (!document.queryCommandSupported("copy")) {
+          return alert("복사하기가 지원되지 않는 브라우저입니다.");
+        }
+
+        const textarea = document.createElement("textarea");
+        textarea.value = text;
+        textarea.style.top = "0";
+        textarea.style.left = "0";
+        textarea.style.position = "fixed";
+
+        document.body.appendChild(textarea);
+        // focus() -> 사파리 브라우저 서포팅
+        textarea.focus();
+        // select() -> 사용자가 입력한 내용을 영역을 설정할 때 필요
+        textarea.select();
+        // 흐름 5.
+        document.execCommand("copy");
+        // 흐름 6.
+        document.body.removeChild(textarea);
+        alert("클립보드에 복사되었습니다.");
+      }
     } catch (error) {
       alert("복사가 실패하였습니다.");
     }
